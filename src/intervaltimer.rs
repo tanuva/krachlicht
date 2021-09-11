e55314f6d7d6bbf82592fc012a5be41fed8fe1c8
Marcel Kummer <tanuva@posteo.de>
2021-09-11 21:46:06 +0200

    Fetch the thread name before usage
    ...instead of during creation. Creation happens on the main thread, thus
    fetching the name before printing FPS is more reliable.

--- src/intervaltimer.rs
+++ src/intervaltimer.rs
@@ -4,7 +4,6 @@ use std::time::{Duration, Instant};
 pub struct IntervalTimer {
     interval: Duration,
     last_tick: Instant,
-    thread_name: String,
     measure_fps: bool,
     last_fps_print: Instant,
     frames: u32,
@@ -13,17 +12,10 @@ pub struct IntervalTimer {
 impl IntervalTimer {
     pub fn new(freq_hz: f32, measure_fps: bool) -> IntervalTimer {
         let frame_duration_microsec = 1000.0 / freq_hz * 1000.0;
-        let cur_thread = thread::current();
-        let thread_name = if let Some(name) = cur_thread.name() {
-            name
-        } else {
-            "unnamed"
-        };
 
         IntervalTimer {
             interval: Duration::from_micros(frame_duration_microsec as u64),
             last_tick: Instant::now(),
-            thread_name: thread_name.to_string(),
             measure_fps,
             last_fps_print: Instant::now(),
             frames: 0,
@@ -50,7 +42,14 @@ impl IntervalTimer {
         self.frames += 1;
 
         if Instant::now() - self.last_fps_print > Duration::from_secs(1) {
-            println!("{} FPS: {}", self.thread_name, self.frames);
+            let cur_thread = thread::current();
+            let thread_name = if let Some(name) = cur_thread.name() {
+                name
+            } else {
+                "unnamed"
+            };
+
+            //println!("{} FPS: {}", thread_name, self.frames);
             self.frames = 0;
             self.last_fps_print = Instant::now();
         }
