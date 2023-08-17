978dbf2d0d0b878eccd208ab57201b924e2733a4
Marcel Kummer <tanuva@posteo.de>
2023-08-17 21:48:02 +0200

    Add first iteration of command line arguments
    Finally! No more switching between SDL and PulseAudio through code
    changes! \o/

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 
+use clap::Parser;
 use olaoutput::OlaOutput;
 use photonizer::Photonizer;
 use playbackstate::PlaybackState;
@@ -24,18 +25,52 @@ use crate::osc::OscReceiver;
 use crate::osc::OscSender;
 use crate::photonizer::PhotonizerOptions;
 
+#[derive(Parser)]
+struct Cli {
+    /// The s16le wav file to play
+    #[arg(short, long, value_name = "FILE")]
+    sound_file_path: Option<std::path::PathBuf>,
+
+    /// The PulseAudio device to listen on
+    #[arg(short, long, value_name = "DEVICE")]
+    pa_device: Option<String>,
+}
+
+fn create_player(
+    args: &Cli,
+    playback_state: Arc<Mutex<PlaybackState>>,
+) -> Result<Box<dyn AudioSource>, &str> {
+    if let Some(sound_file_path) = args.sound_file_path.as_deref() {
+        return Ok(Box::new(SDLPlayer::new(
+            sound_file_path.to_str().unwrap(),
+            Arc::clone(&playback_state),
+        )));
+    };
+
+    if let Some(pa_device) = args.pa_device.as_deref() {
+        return Ok(Box::new(PulseInput::new(
+            Arc::clone(&playback_state),
+            pa_device,
+        )));
+    };
+
+    return Err("No PulseAudio device or audio file given");
+}
+
 fn main() {
+    let args = Cli::parse();
+
     let osc_listen_addr = SocketAddr::from_str("0.0.0.0:8000").unwrap();
     let osc_dst_addr = SocketAddr::from_str("192.168.0.141:9000").unwrap();
 
-    let window_size = 1024;
-    let playback_state = Arc::new(Mutex::new(PlaybackState::new(window_size)));
     let photonizer_options = Arc::new(Mutex::new(PhotonizerOptions::new()));
 
-    let file_path = "/Users/marcel/Downloads/tmt_s16le.wav";
-    let player = SDLPlayer::new(file_path, Arc::clone(&playback_state));
-    //let device = "1__Channel_2.monitor".to_string();
-    //let mut player = PulseInput::new(Arc::clone(&playback_state), device);
+    let window_size = 1024;
+    let playback_state = Arc::new(Mutex::new(PlaybackState::new(window_size)));
+    let mut player = match create_player(&args, Arc::clone(&playback_state)) {
+        Ok(player) => player,
+        Err(err) => panic!("Cannot set up audio source: {}", err),
+    };
 
     let ola_addr = SocketAddr::from_str("127.0.0.1:7770").unwrap();
     let ola = match OlaOutput::new(ola_addr) {
